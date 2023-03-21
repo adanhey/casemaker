50aad2a3979c1451bc45a35a19a9d3cdbc8f32ac
adan <609998924@qq.com>
2023-03-21 15:27:38 +0800

    fin

--- SQLconnect/table_creater.py
+++ SQLconnect/table_creater.py
@@ -8,82 +8,80 @@ app.config.from_object(SQLconnect.DBconfig)
 db = SQLAlchemy(app)
 
 
-# 创建表模型类对象
-#
-#
+
+class Project(db.Model):
+    __tablename__ = 'project'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
 
 
-# class Project(db.Model):
-#     __tablename__ = 'project'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     name = db.Column(db.String(50), nullable=False)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class ModelPart(db.Model):
-#     __tablename__ = 'modelPart'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     project_id = db.Column(db.Integer, nullable=False)
-#     name = db.Column(db.String(50), nullable=False)
-#     quote = db.Column(db.String(50), nullable=False)
-#     quoted = db.Column(db.String(50), nullable=False)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class ServerSideData(db.Model):
-#     __tablename__ = 'serverSide'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class InterfaceSetting(db.Model):
-#     __tablename__ = 'interfaceSetting'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class ClientServer(db.Model):
-#     __tablename__ = 'clientServer'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class WebClient(db.Model):
-#     __tablename__ = 'webClient'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class AppClient(db.Model):
-#     __tablename__ = 'appClient'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class IotDataCollect(db.Model):
-#     __tablename__ = 'iotDataCollect'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
-#
-#
-# class IotDataIssue(db.Model):
-#     __tablename__ = 'iotDataIssue'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     modelPartId = db.Column(db.Integer, nullable=False)
-#     jsonData = db.Column(db.JSON, nullable=True)
-#     storage_time = db.Column(db.DateTime, default=datetime.now)
+class ModelPart(db.Model):
+    __tablename__ = 'modelPart'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    project_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    quote = db.Column(db.JSON, nullable=False)
+    quoted = db.Column(db.JSON, nullable=False)
+    module_special_check = db.Column(db.JSON, nullable=False)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+    be_quote = db.Column(db.JSON, nullable=False)
+
+
+class ServerSideData(db.Model):
+    __tablename__ = 'serverSide'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class InterfaceSetting(db.Model):
+    __tablename__ = 'interfaceSetting'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class ClientServer(db.Model):
+    __tablename__ = 'clientServer'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class WebClient(db.Model):
+    __tablename__ = 'webClient'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class AppClient(db.Model):
+    __tablename__ = 'appClient'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class IotDataCollect(db.Model):
+    __tablename__ = 'iotDataCollect'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class IotDataIssue(db.Model):
+    __tablename__ = 'iotDataIssue'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    modelPartId = db.Column(db.Integer, nullable=False)
+    jsonData = db.Column(db.JSON, nullable=True)
+    storage_time = db.Column(db.DateTime, default=datetime.now)
 
 class PublicCase(db.Model):
     __tablename__ = 'publicCase'
